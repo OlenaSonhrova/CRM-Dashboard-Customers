f5f190fe8926c8cb64880cfaae9d44775ec39e05
Olena Sonhrova <esongrovaa@gmail.com>
2024-11-02 14:35:09 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+	var burgerMenu = document.querySelector('.burger__menu');
+	var dashboardSidebar = document.querySelector('.dashboard__sidebar');
+	var sidebarLogoText = document.querySelector('.sidebar__logo-text');
+	var body = document.body;
+
+	burgerMenu.addEventListener('click', function (event) {
+		burgerMenu.classList.toggle('active');
+		dashboardSidebar.classList.toggle('active');
+		sidebarLogoText.classList.toggle('active');
+		body.classList.toggle('lock');
+	});
+});
